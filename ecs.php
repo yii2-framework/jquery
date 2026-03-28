cbb6bfd5d366ce33238fa37a889aa7560a333776
Wilmer Arambula <terabytesoftw@gmail.com>
2026-03-28 10:16:58 -0300

    Fixed ECS Ci.

--- ecs.php
+++ ecs.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+
 /** @var \Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder $ecsConfigBuilder */
 $ecsConfigBuilder = require __DIR__ . '/vendor/php-forge/coding-standard/config/ecs.php';
 
-return $ecsConfigBuilder->withPaths(
-    [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ],
-);
+return $ecsConfigBuilder
+    ->withSkip(
+        [
+            FullyQualifiedStrictTypesFixer::class,
+        ],
+    )
+    ->withPaths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    );
